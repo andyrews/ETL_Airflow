16a435bc325cabaeaeab605728236482b6df7b47
andyrews <john.andrew.gacho@gmail.com>
2025-06-04 21:45:14 +0800

    Formatted

--- airflow_test_beginner/dags/first_dag.py
+++ airflow_test_beginner/dags/first_dag.py
@@ -14,24 +14,19 @@ with DAG(
     dag_id="sample_dag_v3",
     description="A simple DAG for demonstration purposes",
     schedule_interval="@daily",
-    start_date=datetime(2025, 6, 3, 12,50),
-    catchup=False
+    start_date=datetime(2025, 6, 3, 12, 50),
+    catchup=False,
 ) as dag:
-    task1 = BashOperator(
-        task_id="first_task",
-        bash_command="echo Hello World!"
-    )
+    task1 = BashOperator(task_id="first_task", bash_command="echo Hello World!")
 
     task2 = BashOperator(
-        task_id="second_task",
-        bash_command="echo This is the second task!"
+        task_id="second_task", bash_command="echo This is the second task!"
     )
 
     task3 = BashOperator(
-        task_id="third_task",
-        bash_command="echo This is the third task!"
+        task_id="third_task", bash_command="echo This is the third task!"
     )
 
-    #task1.set_downstream(task2)
-    #task1.set_downstream(task3)
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
     task1 >> [task2, task3]
